afe9882605d8438c63262d24d9fc3a9cddf093ff
Abhinav Tripathi <abhirishitri@gmail.com>
2020-07-19 14:35:32 +0530

    Add data binding and introduce Login fragment

--- app/src/main/java/com/abhi/quickappointment/view/LoginFragment.kt
+++ app/src/main/java/com/abhi/quickappointment/view/LoginFragment.kt
@@ -1,31 +1,30 @@
 package com.abhi.quickappointment.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.abhi.quickappointment.R
+import com.abhi.quickappointment.databinding.LoginFragmentBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = LoginFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
